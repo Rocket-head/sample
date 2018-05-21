b3f1b1f518ec64154010d808440edd16da08ffea
rocket-head <pasaweena@gmail.com>
2018-05-21 12:34:08 -0700

    permute and combi

--- sample-git/main.cpp
+++ sample-git/main.cpp
@@ -1,9 +1,12 @@
 #include <iostream>
 
 using namespace std;
+int mainper();
 
 int main()
 {
     cout << "Hello world!" << endl;
+    cout << "this is tutorial c++" << endl;
+    mainper();
     return 0;
 }
